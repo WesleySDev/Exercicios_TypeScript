3c5b4713c190d41564ffb6d82c3a3a87e1ae544f
WesleySDev <wesleydasilvagit@gmail.com>
2025-07-16 18:39:22 -0300

    Criação de  exercicios para estudar

---
+++ Estruturas sequenciais/Ex - 01/exercicio.js
@@ -0,0 +1,5 @@
+/*
+Faça um Programa que mostra a mensagem "Alo mundo" na tela.
+*/
+var mensagem = "Alo mundo";
+console.log(mensagem);
---
+++ Estruturas sequenciais/Ex - 01/exercicio.ts
@@ -0,0 +1,6 @@
+/*
+Faça um Programa que mostra a mensagem "Alo mundo" na tela.
+*/
+
+let mensagem: string = "Alo mundo";
+console.log(mensagem);
