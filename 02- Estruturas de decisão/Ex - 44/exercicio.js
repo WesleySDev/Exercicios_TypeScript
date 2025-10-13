8d256e5519943b86fcfe3ed8486bbf8b155f338e
WesleySDev <wesleydasilvagit@gmail.com>
2025-10-12 22:36:57 -0300

    feat: adicionar exercício de contagem de 1 a 10 com loop

--- 02- Estruturas de decisão/Ex - 44/exercicio.js
+++ 02- Estruturas de decisão/Ex - 44/exercicio.js
@@ -1,1 +1,13 @@
-"use strict";
+/*Contagem de 1 a 10
+
+Descrição: Utilize um loop para exibir os números de 1 a 10.
+
+Exemplo de Output:
+
+1 2 3 4 5 6 7 8 9 10
+ */
+for (var i = 1; i <= 10; i++) {
+    process.stdout.write(i + " "); // Exibe os números na mesma linha
+}
+console.log(); // Adiciona uma nova linha após a contagem
+console.log("\nContagem conclu\u00EDda!"); // Mensagem de conclusão
---
+++ 02- Estruturas de decisão/Ex - 44/exercicio.ts
@@ -0,0 +1,14 @@
+/*Contagem de 1 a 10
+
+Descrição: Utilize um loop para exibir os números de 1 a 10.
+
+Exemplo de Output:
+
+1 2 3 4 5 6 7 8 9 10
+ */
+
+for (let i: number = 1; i <= 10; i++) {
+  process.stdout.write(i + " "); // Exibe os números na mesma linha
+}
+console.log(); // Adiciona uma nova linha após a contagem
+console.log(`\nContagem concluída!`); // Mensagem de conclusão
